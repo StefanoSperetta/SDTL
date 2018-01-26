10b93da9387473ce82308b2b1417c8498ac61fc5
Stefano Speretta <s.speretta@tudelft.nl>
2018-01-26 17:10:01 +0100

    Handle wildcards in path

--- src/main/java/demo/ImportPyTrack.java
+++ src/main/java/demo/ImportPyTrack.java
@@ -5,10 +5,17 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.List;
 
 import org.apache.log4j.Logger;
 
@@ -17,56 +24,78 @@ import SDTL.Protocol.DownlinkFrame;
 
 public class ImportPyTrack 
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 	{
-		String filename = "/Users/stefanosperett/Documents/Delfi-C3/Telemetry/PyTrack/20180113_085605z_145868900_32789_packets.log";
+		String filename = "/Users/stefanosperett/Documents/Delfi-C3/Telemetry/PyTrack/201801*_145868900_32789_packets.log";
+		
+		Path p = Paths.get(filename);
+		Path n = p.getFileName();
+		String name = n.toString();
+		//tring dir  = filename.substring(0, filename.length() - name.length());
 		
 		DateFormat df = new SimpleDateFormat("yyyy-MM-dd kk:mm:ss.SSS z");
+		List<Path> directory = new ArrayList<Path>();
 		
-		try(BufferedReader br = new BufferedReader(new FileReader(filename))) 
+		int counter = 0;
+		FileWriter<DownlinkFrame> fw = new FileWriter<DownlinkFrame>(DownlinkFrame.SCHEMA$, "frames.avro");
+		
+		
+		try (DirectoryStream<Path> dirStream = Files.newDirectoryStream(Paths.get(p.getParent().toString()), p.getFileName().toString()))
 		{
-			int index = 0;
-			FileWriter<DownlinkFrame> fw = new FileWriter<DownlinkFrame>(DownlinkFrame.SCHEMA$, "frames.avro");
-			
-		    for(String line; (line = br.readLine()) != null; ) 
-		    {
-		        // process the line.
-		    		System.out.println(line);
-		    		String[] separated = line.split(",");		    		
-		    		
-		    		Date result =  df.parse(separated[0]); 
-		    		ByteBuffer data = ByteBuffer.allocate(separated[1].length() / 2);
-
-		    		for (int i = 0; i < separated[1].length(); i+=2)
-		    		{
-		    			data.put((byte)Integer.parseInt(separated[1].substring(i, i+2), 16));
-		    			//System.out.print( String.format("%02X", data[i / 2]) + " ");
-		    		}
-		    		data.flip();
-		    		System.out.println();
-		    		System.out.println(result + " --- " + separated[1] );
-		    		
-		    		DownlinkFrame f0 = DownlinkFrame.newBuilder()
-	    				.setId(index++)
-	    				.setTimestamp(result.getTime())
-	    				.setData(data)
-	    				.build();
-		    		
-		    		fw.append(f0);
-		    		System.out.println(f0);
-		    }
-		    
-		    fw.close();
-		    // line is not visible here.
-		} catch (FileNotFoundException e) 
+			//dirStream.forEach(path -> System.out.println(path));
+			dirStream.forEach(directory::add);
+		}
+		directory.sort(Comparator.comparing(Path::toString));
+		for (Path entry : directory)
 		{
-			Logger.getLogger(ImportPyTrack.class).error("File not found.", e);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
+			System.out.println("----> " + entry);
+		
+			try(BufferedReader br = new BufferedReader(new FileReader(entry.toFile()))) 
+			{
+				
+			    for(String line; (line = br.readLine()) != null; ) 
+			    {
+			        // process the line.
+			    		System.out.println(line);
+			    		String[] separated = line.split(",");		    		
+			    		
+			    		Date result =  df.parse(separated[0]); 
+			    		ByteBuffer data = ByteBuffer.allocate(separated[1].length() / 2);
+	
+			    		for (int i = 0; i < separated[1].length(); i+=2)
+			    		{
+			    			data.put((byte)Integer.parseInt(separated[1].substring(i, i+2), 16));
+			    		}
+			    		data.flip();
+	
+			    		System.out.println(result + " --- " + separated[1] );
+			    		
+			    		DownlinkFrame f0 = DownlinkFrame.newBuilder()
+		    				.setTimestamp(result.getTime())
+		    				.setData(data)
+		    				.build();
+			    		
+			    		fw.append(f0);
+			    		System.out.println(f0);
+			    		counter++;
+			    }
+			    
+			    
+			    
+			    // line is not visible here.
+			} catch (FileNotFoundException e) 
+			{
+				Logger.getLogger(ImportPyTrack.class).error("File not found.", e);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
+		}
+		System.out.println();
+	    System.out.println("Added " + counter + " frames");
+		fw.close();
 	}
 }
